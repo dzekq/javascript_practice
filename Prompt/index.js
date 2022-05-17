a874317b81bdbd0d705bccbadd116ea2262c7ab0
dzekq <dzekaskola@gmail.com>
2022-05-17 20:32:21 +0200

    Added BeginnerFunctions folder

---
+++ Prompt/index.js
@@ -0,0 +1,12 @@
+// var a = 5
+// var nekiText = "Neki Text";
+// console.log(a + 2,5);
+
+// var unosVrednosti = prompt("Unesite broj.");
+// console.log(parseInt(unosVrednosti) + 5);
+var unosVrednosti = prompt("Godina u datom trenutku");
+var svakaPrestupnaGodina = 4;
+var textZaConsolu = ("Dosad je prestupnih godina bilo:");
+var prestupnaGodina = parseInt(unosVrednosti) / parseInt(svakaPrestupnaGodina);
+
+console.log(textZaConsolu + prestupnaGodina)
